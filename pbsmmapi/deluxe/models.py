b31a1f2ce9161e4582e9f3fd3b024a06cd47750c
Bob Donahue <bob_donahue@wgbh.org>
2018-05-09 11:24:54 -0400

    Still trying to get deluxe to be recognized

--- pbsmmapi/deluxe/models.py
+++ pbsmmapi/deluxe/models.py
@@ -1,8 +1,8 @@
-import .asset.models
-import .collection.models
-import .episode.models
-import .franchise.models
-import .remoteasset.models
-import .season.models
-import .show.models
-import .special.models
+import asset.models
+import collection.models
+import episode.models
+import franchise.models
+import remoteasset.models
+import season.models
+import show.models
+import special.models
