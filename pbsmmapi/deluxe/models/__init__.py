264ca0a69866539bf1f4b7cfe27f2c0ff1e1c770
Bob Donahue <bob_donahue@wgbh.org>
2018-05-09 12:13:01 -0400

    Still trying to get deluxe to be recognized

---
+++ pbsmmapi/deluxe/models/__init__.py
@@ -0,0 +1,18 @@
+from ..asset.models import PBSMMAsset
+from ..collection.models import PBSMMCollection
+from ..episode.models import PBSMMEpisode
+from ..franchise.models import PBSMMFranchise
+from ..remoteasset.models import PBSMMRemoteAsset
+from ..season.models import PBSMMSeason
+from ..show.models import PBSMMShow
+from ..special.models import PBSMMSpecial
+
+__all__ = [
+    'PBSMMAsset',
+    'PBSMMCollection',
+    'PBSMMEpisode',
+    'PBSMMFranchise',
+    'PBSMMSeason',
+    'PBSMMSeason',
+    'PBSMMSpecial',
+]
