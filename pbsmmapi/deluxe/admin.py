82b4c4d18e3f9bf3a0f850e31c627e8df29c090b
Bob Donahue <bob_donahue@wgbh.org>
2018-05-09 10:41:22 -0400

    adding admin

---
+++ pbsmmapi/deluxe/admin.py
@@ -0,0 +1,8 @@
+import asset.admin
+import collection.admin
+import episode.admin
+import franchise.admin
+import remoteasset.admin
+import season.admin
+import show.admin
+import special.admin
--- pbsmmapi/deluxe/models.py
+++ pbsmmapi/deluxe/models.py
@@ -3,5 +3,6 @@ import collection.models
 import episode.models
 import franchise.models
 import remoteasset.models
+import season.models
 import show.models
 import special.models
