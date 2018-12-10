f12458ea245fa9cbedce45a1f1c35aa2da8200ec
Jay Thompson <jay_thompson@wgbh.org>
2018-12-10 09:14:26 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pbsmmapi',
-    version='0.1',
-    packages=find_packages(),
+    version='0.2',
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    license='MIT License, see LICENSE',  # example license
+    license='MIT License, see LICENSE',
     description="Django models that import content from the PBS MediaManager API",
     long_description=README,
     url='http://github.com/wgbh/django-pbsmmapi/',
@@ -23,19 +23,20 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  # example license
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.4',
-        #'Programming Language :: Python :: 3.5',
-        #'Topic :: Internet :: WWW/HTTP',
-        #'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
