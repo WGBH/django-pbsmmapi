e324ba676ec9dab5f20d599ac04cef7a9d772006
Jay Thompson <jay_thompson@wgbh.org>
2019-12-10 14:34:02 -0600

    Fix for change in show/season list API endpoint
    PBS removed the link back to a Show from the season listing endpoint.
    This update gets the full season record before doing the ingest.

--- pbsmmapi/season/ingest_season.py
+++ pbsmmapi/season/ingest_season.py
@@ -1,3 +1,4 @@
+from ..api.api import get_PBSMM_record
 from ..abstract.helpers import set_json_serialized_field, fix_non_aware_datetime
 
 
@@ -5,6 +6,10 @@ def process_season_record(obj, instance, origin='season'):
     """
     Take the data returned from a single Season's API JSON content and map it to a PBSMMEpisode database record.
     """
+    # We have to get the detail endpoint now because PBS removed the show link from season listings.
+    self_link = obj['links']['self']
+    status, obj = get_PBSMM_record(self_link)
+
     # These are the top-level fields - almost everything else is under attrs
     if 'attributes' not in obj.keys():
         attrs = obj['data']['attributes']
@@ -15,13 +20,14 @@ def process_season_record(obj, instance, origin='season'):
 
     # UUID and updated_on
     if 'id' in obj.keys():
-        instance.object_id = obj.get('id', None)  # This should always be set.
+        instance.object_id = obj.get('id', None)    # This should always be set.
     else:
         instance.object_id = obj['data'].get('id')
 
     instance.updated_at = fix_non_aware_datetime(
-        attrs.get('updated_at', None))  # timestamp of the record in the API
-    instance.api_endpoint = links.get('self', None)  # URL of the request
+        attrs.get('updated_at', None)
+    )    # timestamp of the record in the API
+    instance.api_endpoint = links.get('self', None)    # URL of the request
 
     # Title, Sortable Ttile, and Slug
     instance.title = attrs.get('title', None)
@@ -32,8 +38,7 @@ def process_season_record(obj, instance, origin='season'):
     instance.description_short = attrs.get('description_short', None)
 
     # Season metadata - things related to the season itself
-    instance.premiered_on = fix_non_aware_datetime(
-        attrs.get('premiered_on', None))
+    instance.premiered_on = fix_non_aware_datetime(attrs.get('premiered_on', None))
     instance.funder_message = attrs.get('funder_message', None)
     instance.is_excluded_from_dfp = attrs.get('is_excluded_from_dfp', None)
     instance.can_embed_player = attrs.get('can_embed_player', None)
@@ -42,8 +47,7 @@ def process_season_record(obj, instance, origin='season'):
     instance.ga_event = attrs.get('tracking_ga_event', None)
     instance.episode_count = attrs.get('episode_count', None)
     instance.display_episode_number = attrs.get('display_episode_number', None)
-    instance.sort_episodes_descending = attrs.get(
-        'sort_episodes_descending', None)
+    instance.sort_episodes_descending = attrs.get('sort_episodes_descending', None)
     instance.ordinal = attrs.get('ordinal', None)
     instance.hashtag = attrs.get('hashtag', None)
 
@@ -53,15 +57,14 @@ def process_season_record(obj, instance, origin='season'):
 
     # The canonical image used for this is the one that has 'mezzanine' in it
     instance.images = set_json_serialized_field(attrs, 'images', default=None)
-    if instance.images is None:  # try latest_asset_images
+    if instance.images is None:    # try latest_asset_images
         instance.images = set_json_serialized_field(
-            attrs, 'latest_asset_images', default=None)
+            attrs, 'latest_asset_images', default=None
+        )
 
-    instance.platforms = set_json_serialized_field(
-        attrs, 'platforms', default=None)
-    instance.audience = set_json_serialized_field(
-        attrs, 'audience', default=None)
-        
+    instance.platforms = set_json_serialized_field(attrs, 'platforms', default=None)
+    instance.audience = set_json_serialized_field(attrs, 'audience', default=None)
+
     # References to parents
     show = attrs.get('show', None)
     instance.show_api_id = show.get('id', None)
