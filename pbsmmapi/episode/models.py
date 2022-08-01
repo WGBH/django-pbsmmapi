eab4268fe542bd282bb0597d7c2018c013a9977b
andrei.popescu <andrei.popescu@3pillarglobal.com>
2022-08-01 15:33:01 +0300

    [THES-44] remove unused code

--- pbsmmapi/episode/models.py
+++ pbsmmapi/episode/models.py
@@ -121,25 +121,6 @@ class PBSMMEpisode(PBSMMGenericEpisode):
         self.post_save(self.id)
 
     def pre_save(self):
-        # object_id = str(self.object_id or "").strip()
-        # if not self.ingest_on_save or self.pk or not object_id:
-        #     return  # we need processing only for new objects
-        # status, json = get_PBSMM_record(f"{PBSMM_EPISODE_ENDPOINT}{object_id}/")
-        # self.object_id = object_id
-        # self.last_api_status = status
-        # self.date_last_api_update = time_zone_aware_now()
-        # if status != HTTPStatus.OK:
-        #     return
-        # attrs = json.get('attributes', json['data'].get('attributes'))
-        # fields = (f.name for f in PBSMMEpisode._meta.get_fields())
-        # for field in (f for f in fields if f != 'id'):
-        #     setattr(self, field, attrs.get(field))
-        # self.updated_at = fix_non_aware_datetime(attrs.get('updated_at'))
-        # self.api_endpoint = json['links'].get('self')
-        # self.links = attrs.get('links')
-        # self.season_api_id = attrs.get('season', dict()).get('id', None)
-        # self.json = json
-        # self.ingest_on_save = False
         self.self_process(PBSMM_EPISODE_ENDPOINT)
 
     @staticmethod
