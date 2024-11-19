8dafe2085b0320de5f026c2fd410580c3023cce2
Rares Mandrean <rares.mandrean@3pillarglobal.com>
2024-11-19 12:52:23 +0200

    THES-326: Finish PBSMMAPIFranchise support

--- pbsmmapi/franchise/apps.py
+++ pbsmmapi/franchise/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FranchiseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "franchise"
+    name = "pbsmmapi.franchise"
--- pbsmmapi/show/tasks.py
+++ pbsmmapi/show/tasks.py
@@ -6,7 +6,7 @@ from pbsmmapi.show.models import Show
 
 
 @db_periodic_task(crontab(minute="0"))
-def scrape_media_manager():
+def scrape_media_manager_shows():
     show_slugs = getattr(settings, "PBSMM_SHOW_SLUGS", [])
     for slug in show_slugs:
         try:
