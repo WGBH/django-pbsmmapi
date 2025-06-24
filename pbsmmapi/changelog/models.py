66d3dc8ab0e021078536ededfecfbaf0700c675b
Jay Thompson <eyemyth@users.noreply.github.com>
2025-06-24 12:32:19 -0500

    big overhaul of changelog processing tasks

--- pbsmmapi/changelog/models.py
+++ pbsmmapi/changelog/models.py
@@ -1,8 +1,7 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.fields.json import KT
 from django.utils.translation import gettext_lazy as _
-
-DT_FORMAT = "%Y-%m-%dT%H:%M:%S.%f%z"
-TO_DT_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 
 class PBSMMResourceType(models.TextChoices):
@@ -34,14 +33,157 @@ class ChangeLog(models.Model):
     # to pick up ingest where we left off
     latest_timestamp = models.DateTimeField(null=True)
 
+    # TODO rename to ingested
     processed = models.BooleanField(default=False)
-    # Not ideal - we should create URL from resource type/content ID
+
+    api_crawled = models.DateTimeField(null=True)
+
+    # TODO: replace field and get_api_url with property
     api_url = models.URLField(null=True)
     api_status = models.IntegerField(null=True)
+    api_data = models.JSONField(default=dict)
+
+    def get_api_url(self):
+        return f"https://media.services.pbs.org/api/v1/{self.resource_type}s/{self.content_id}/"
 
     def save(self, *args, **kwargs):
         self.latest_timestamp = max(self.entries.keys(), default=None)
+        if self.get_instance() is not None:
+            self.processed = True
         super().save(*args, **kwargs)
+
+    def get_content_type(self):
+        return ContentType.objects.get(
+            app_label=self.resource_type,
+            model=self.resource_type,
+        )
+
+    def get_model_class(self):
+        try:
+            ct = ContentType.objects.get(
+                app_label=self.resource_type,
+                model=self.resource_type,
+            )
+            return ct.model_class()
+        except ContentType.DoesNotExist:
+            return None
+
+    def get_instance(self):
+        # try to get a previously saved instance
+        model = self.get_model_class()
+        assert model is not None
+        try:
+            return model.objects.get(object_id=self.content_id)
+        except model.DoesNotExist:
+            return None
+
+    def __str__(self):
+        return f"Changelog for {self.resource_type} {self.content_id}"
 
     class Meta:
         ordering = ["latest_timestamp"]
+
+
+class ShowChangeLogManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(resource_type="show")
+
+
+class ShowChangeLog(ChangeLog):
+    objects = ShowChangeLogManager()
+
+    class Meta:
+        proxy = True
+
+
+class SeasonChangeLogManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .filter(resource_type="season")
+            .annotate(show_id=KT("api_data__data__attributes__show__id"))
+        )
+
+
+class SeasonChangeLog(ChangeLog):
+    objects = SeasonChangeLogManager()
+
+    class Meta:
+        proxy = True
+
+
+class EpisodeChangeLogManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .filter(resource_type="episode")
+            .annotate(show_id=KT("api_data__data__attributes__show__id"))
+        )
+
+
+class EpisodeChangeLog(ChangeLog):
+    objects = EpisodeChangeLogManager()
+
+    class Meta:
+        proxy = True
+
+
+class SpecialChangeLogManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .filter(resource_type="special")
+            .annotate(show_id=KT("api_data__data__attributes__show__id"))
+        )
+
+
+class SpecialChangeLog(ChangeLog):
+    objects = SpecialChangeLogManager()
+
+    class Meta:
+        proxy = True
+
+
+class AssetChangeLogManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .filter(resource_type="asset")
+            .annotate(
+                parent_type=KT("api_data__data__attributes__parent_tree__type"),
+                parent_id=KT("api_data__data__attributes__parent_tree__id"),
+            )
+        )
+
+
+class AssetChangeLog(ChangeLog):
+    objects = AssetChangeLogManager()
+
+    def get_parent_model_class(self):
+        try:
+            ct = ContentType.objects.get(
+                app_label=self.parent_type,
+                model=self.parent_type,
+            )
+            return ct.model_class()
+        except ContentType.DoesNotExist:
+            return None
+
+    def get_parent_instance(self):
+        # try to get a previously saved instance
+        model = self.get_parent_model_class()
+        assert model is not None
+        try:
+            return model.objects.get(object_id=self.parent_id)
+        except model.DoesNotExist:
+            return None
+
+    def __str__(self):
+        return f"AssetChangelog for {self.content_id} - parent {self.get_parent_instance()}"
+
+    class Meta:
+        proxy = True
--- pbsmmapi/changelog/tasks.py
+++ pbsmmapi/changelog/tasks.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from collections.abc import Iterable
 from datetime import (
+    UTC,
     datetime,
     timedelta,
 )
@@ -10,6 +11,12 @@ from urllib.parse import (
     urlparse,
 )
 
+from django.db.models import (
+    Exists,
+    F,
+    OuterRef,
+)
+from django.db.models.lookups import LessThan
 from huey import crontab
 from huey.contrib.djhuey import (
     db_periodic_task,
@@ -19,7 +26,19 @@ from huey.contrib.djhuey import (
 )
 
 from pbsmmapi.api.api import get_PBSMM_record
-from pbsmmapi.changelog.models import ChangeLog
+from pbsmmapi.asset.models import Asset
+from pbsmmapi.changelog.models import (
+    AssetChangeLog,
+    ChangeLog,
+    EpisodeChangeLog,
+    SeasonChangeLog,
+    SpecialChangeLog,
+)
+from pbsmmapi.episode.models import Episode
+from pbsmmapi.franchise.models import Franchise
+from pbsmmapi.season.models import Season
+from pbsmmapi.show.models import Show
+from pbsmmapi.special.models import Special
 
 BASE_CHANGELOG_URL = "https://media.services.pbs.org/api/v1/changelog/?sort=timestamp&type=asset&type=episode&type=franchise&type=season&type=show&type=special"
 
@@ -36,7 +55,8 @@ def default_changelog_dict():
 
 def prep_changelog_data(entries: Iterable[dict]) -> dict:
     """
-    Group Changelog entries by UUID.
+    Group Changelog entries by UUID; combine attributes into a dict with
+    timestamps as keys.
     """
     combined = defaultdict(default_changelog_dict)
     for changelog_dict in entries:
@@ -51,6 +71,10 @@ def prep_changelog_data(entries: Iterable[dict]) -> dict:
 
 @db_task(retries=3)
 def save_changelog_entries(combined: dict):
+    """
+    Using unified dict returned from prep_changelog_data, save ChangeLog
+    instances for each content ID extracted from the changelog endpoint.
+    """
     for content_id, data in combined.items():
         try:
             log = ChangeLog.objects.get(content_id=content_id)
@@ -72,6 +96,9 @@ def get_changelog_entries(url: str) -> list[dict]:
 
 
 def max_page_number(mm_response_data: dict) -> int:
+    """
+    Ensure we only fetch 400 changelog pages per minute.
+    """
     links: dict = mm_response_data.get("links", dict())
     last: str = links.get("last", "")
     parsed = urlparse(last)
@@ -85,6 +112,150 @@ def max_page_number(mm_response_data: dict) -> int:
         return MAX_QUERIES + 1
     else:
         return last_page + 1
+
+
+@db_task(retries=3)
+def fetch_api_data(log: ChangeLog):
+    status, data = get_PBSMM_record(log.get_api_url())
+    log.api_status = status
+    log.api_crawled = datetime.now(UTC)
+    if status == 200:
+        log.api_data = data
+    log.save()
+
+
+def ingest_new_objects():
+    # note: Franchise Shows are ingested during reingest_updated_objects
+    for show in Show.objects.all():
+        season_logs = SeasonChangeLog.objects.filter(
+            show_id=show.object_id,
+            processed=False,
+        )
+        for log in season_logs:
+            if log.get_instance() is not None:
+                log.save()
+            else:
+                Season(object_id=log.content_id).save()
+
+        episode_logs = EpisodeChangeLog.objects.filter(
+            show_id=show.object_id,
+            processed=False,
+        )
+        for log in episode_logs:
+            if log.get_instance() is not None:
+                log.save()
+            else:
+                Episode(object_id=log.content_id).save()
+
+        special_logs = SpecialChangeLog.objects.filter(
+            show_id=show.object_id,
+            processed=False,
+        )
+        for log in special_logs:
+            if log.get_instance() is not None:
+                log.save()
+            else:
+                Special(object_id=log.content_id).save()
+
+    for log in AssetChangeLog.objects.exclude(parent_type__isnull=True):
+        parent = log.get_parent_instance()
+        if parent is not None:
+            if not parent.assets.filter(object_id=log.content_id).exists():
+                parent.ingest_on_save = True
+                parent.save()
+
+
+def reingest_updated_objects():
+    for franchise in Franchise.objects.filter(
+        Exists(ChangeLog.objects.filter(content_id=OuterRef("object_id")))
+    ):
+        changelog = ChangeLog.objects.get(content_id=franchise.object_id)
+        if changelog.latest_timestamp > franchise.date_last_api_update:
+            print("-" * 80)
+            print(f"updating {franchise}")
+            print("-" * 80)
+            franchise.ingest_on_save = True
+            franchise.ingest_shows = True
+            franchise.save()
+
+    querysets = [
+        Show.objects.filter(
+            Exists(ChangeLog.objects.filter(content_id=OuterRef("object_id")))
+        ),
+        Special.objects.filter(
+            Exists(ChangeLog.objects.filter(content_id=OuterRef("object_id")))
+        ),
+        Season.objects.filter(
+            Exists(ChangeLog.objects.filter(content_id=OuterRef("object_id")))
+        ),
+        Episode.objects.filter(
+            Exists(ChangeLog.objects.filter(content_id=OuterRef("object_id")))
+        ),
+        Asset.objects.filter(
+            Exists(ChangeLog.objects.filter(content_id=OuterRef("object_id")))
+        ),
+    ]
+    for queryset in querysets:
+        for item in queryset:
+            changelog = ChangeLog.objects.get(content_id=item.object_id)
+            if changelog.latest_timestamp > item.date_last_api_update:
+                print("-" * 80)
+                print(f"updating {item}")
+                print("-" * 80)
+                item.ingest_on_save = True
+                item.save()
+
+
+def get_changelog_data(limit: int):
+    """
+    For ChangeLog objects we can't match with an ingested object, we
+    need to fetch the API data in order to determine whether to ingest
+    the object.
+    """
+    # for changelogs without API data
+    logs = ChangeLog.objects.filter(
+        api_status__isnull=True,
+        processed=False,
+    )
+
+    if logs.count() > limit:
+        logs = logs[:limit]
+        limit = 0
+    else:
+        limit = limit - logs.count()
+
+    fetch_api_data.map(logs)
+
+    # retry API fetch for objects that previously returned 403 or 404,
+    # and which have been updated since the last API fetch attempt
+    if limit > 0:
+        logs = ChangeLog.objects.filter(
+            api_status__in=[403, 404],
+        ).filter(
+            LessThan(
+                F("api_crawled"),
+                F("latest_timestamp"),
+            )
+        )
+
+        print("-" * 80)
+        print(f"{logs.count()} 403/404 logs to update")
+        print(f"{logs.count() / MAX_QUERIES} minutes remaining")
+        print("-" * 80)
+
+        if logs.count() > limit:
+            logs = logs[:limit]
+            limit = 0
+        else:
+            limit = limit - logs.count()
+
+        fetch_api_data.map(logs)
+
+    # at this point it's unlikely that we'll need to worry about going over the
+    # API limit so we should just ingest new objects and update existing ones
+    if limit > 0:
+        reingest_updated_objects()
+        ingest_new_objects()
 
 
 @db_periodic_task(crontab(minute="*/1"))
@@ -104,7 +275,6 @@ def scrape_changelog():
             DT_FORMAT,
         )
         base_url = f"{BASE_CHANGELOG_URL}&since={since}"
-
         _, mm_response_data = get_PBSMM_record(base_url)
         upper_page_bound = max_page_number(mm_response_data)
         urls = [f"{base_url}&page={i}" for i in range(1, upper_page_bound)]
@@ -112,3 +282,6 @@ def scrape_changelog():
     entries = get_changelog_entries.map(urls)
     data = prep_changelog_data(chain.from_iterable(entries.get(blocking=True)))
     save_changelog_entries(data)
+
+    remaining_api_calls = MAX_QUERIES - len(urls)
+    get_changelog_data(remaining_api_calls)
