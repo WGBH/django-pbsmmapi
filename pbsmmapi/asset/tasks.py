d807839b0a0b6692ef70a9f93ed1d86a84d019d0
Jay Thompson <eyemyth@users.noreply.github.com>
2026-07-21 12:21:13 -0500

    update_partial_assets: exclude deleted and ensure ordering'

--- pbsmmapi/asset/tasks.py
+++ pbsmmapi/asset/tasks.py
@@ -18,7 +18,8 @@ def update_partial_assets() -> None:
     incomplete_assets = Asset.objects.filter(
         data_format="compact",
         mm_content__last_api_status=200,
-    )
+        mm_content__deleted__isnull=True,
+    ).order_by("pk")
     for asset in incomplete_assets[:100]:
         asset.ingest_on_save = True
         asset.save()
